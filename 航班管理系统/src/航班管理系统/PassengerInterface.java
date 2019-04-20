64acddd968f5daacf2f5897c5ae7756dd783b9f6
DELL <DELL@DESKTOP-O1MTQI6.lan>
2019-04-20 18:28:33 +0800

    第一次提交

--- 航班管理系统/src/航班管理系统/PassengerInterface.java
+++ 航班管理系统/src/航班管理系统/PassengerInterface.java
@@ -1,6 +1,28 @@
 package 航班管理系统;
-
-
-public class PassengerInterface {
-
+/**
+ * 旅客接口
+ * @author 吴昊男 1063256708@qq.com
+ * @version v1.0 Copyright(C),2019-2020
+ * */
+public interface PassengerInterface {
+	/**
+	 * 获取旅客姓名
+	 * @return 返回姓名
+	 */
+	public String getName();  //获取旅客姓名
+	/**
+	 * 获取预定号
+	 * @return 返回预定号
+	 */
+	public int getBookingNumber() ;//获取预定号
+	/**
+	 * 获取座位排数
+	 * @return 返回排数
+	 */
+	public int getRow();//获取座位排数
+	/**
+	 * 获取座位号
+	 * @return 返回座位号
+	 */
+	public int getSeatPosition();//获取座位号
 }
